d738da48a1328d6e60c3ef0d131aeb2e081640dd
Brendon Ban <brendon.ban@outlook.com>
2025-11-25 22:19:52 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -178,13 +178,13 @@ export default function Personal() {
       >
         <h3 className="mb-5 text-lg font-medium">Education</h3>
         <div className="flex flex-col space-y-2">
-          {EDUCATION.map((job) => (
+          {EDUCATION.map((education) => (
             <a
               className="relative overflow-hidden rounded-2xl bg-zinc-300/30 p-[1px] dark:bg-zinc-600/30"
-              href={job.link}
+              href={education.link}
               target="_blank"
               rel="noopener noreferrer"
-              key={job.id}
+              key={education.id}
             >
               <Spotlight
                 className="from-zinc-900 via-zinc-800 to-zinc-700 blur-2xl dark:from-zinc-100 dark:via-zinc-200 dark:to-zinc-50"
@@ -194,14 +194,14 @@ export default function Personal() {
                 <div className="relative flex w-full flex-row justify-between">
                   <div>
                     <h4 className="font-normal dark:text-zinc-100">
-                      {job.title}
+                      {education.degree}
                     </h4>
                     <p className="text-zinc-500 dark:text-zinc-400">
-                      {job.company}
+                      {education.university}
                     </p>
                   </div>
                   <p className="text-zinc-600 dark:text-zinc-400">
-                    {job.start} - {job.end}
+                    {education.start} - {education.end}
                   </p>
                 </div>
               </div>
